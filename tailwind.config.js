e98c010eb381c4efec8eba096bd6c1526469d48e
Harry Lee <101452696+harryleevn93@users.noreply.github.com>
2022-11-14 19:19:00 +0700

    finished practice

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: { 
+    enabled: true, 
+    content: ['./dist/**/*.html'], 
+  }, 
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'title': ['Sansita', 'sans-serif'],
+        'body' : ['Lato','sans-serif']
+      },
+      colors: {
+        'pri': '#081F4D',
+        'sec': '#0083FF',
+        'gen': '#3B4D71',
+        'our-bg': '#D9E6FF'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
